f12a25665a6b468e8622b5def46bf8494116f886
neal <neal.armstrong999@gmail.com>
2023-05-28 10:48:31 +0800

    初始化程序

--- src/main/java/com/h4p3/MemberReactiveApplication.java
+++ src/main/java/com/h4p3/MemberReactiveApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MemberReactiveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MemberReactiveApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MemberReactiveApplication.class, args);
+    }
 
 }
